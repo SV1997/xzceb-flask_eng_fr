962a754b32acf99d2214ea6b0b2071ce07f6fec0
SV1997 <96911906+SV1997@users.noreply.github.com>
2022-01-02 17:57:09 +0530

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,26 @@
-from machinetranslation import translator
+import translate.languagetr
 from flask import Flask, render_template, request
 import json
+# from flask_sqlalchemy import SQLAlchemy
 
-app = Flask("Web Translator")
-
-@app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
-
-@app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+app = Flask(__name__,template_folder='template')
 
 @app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+def renderIndexPage(): # Write the code to render template
+    return render_template('index.html')
+@app.route("/englishToFrench")
+def translateToFrench():
+    textToTranslate = request.form.get('textToTranslate')
+    t=translate.languagetr.en2fr(textToTranslate)
+    return t
+    # return "Translated text to French"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+@app.route("/frenchToEnglish")
+def translateToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    translated_text=translate.languagetr.fr2en(textToTranslate)# Write your code here
+
+    return f"{translated_text}"
+
+
+app.run(host="0.0.0.0", port=8080,debug=True)
